253f232e65515c9edbdebe69bd41cec40d174c18
Amin Nademi <mgamer7557@gmail.com>
2024-10-26 22:31:54 +0330

    Implement asexual and sexual reproduction and genetic combination methods. Offspring and child are generated based on specified genetic similarity rules.

--- Animal.cpp
+++ Animal.cpp
@@ -40,3 +40,69 @@ bool Animal::operator==(const Animal &other) const
 
     return calculateGeneticSimilarity(other) > 70.0;
 }
+
+Animal Animal::asexualReproduction()
+{
+    vector<Genome> doubledChromosomes = chromosomes;
+
+    for (const auto &chromosome : chromosomes)
+        doubledChromosomes.push_back(chromosome);
+
+    vector<pair<string, string>> selectedChromosomes;
+    double similarity = 0.0;
+
+    while (similarity <= 70.0)
+    {
+        selectedChromosomes.clear();
+        for (size_t i = 0; i < chromosomes.size(); i++)
+        {
+            int idx = rand() % doubledChromosomes.size();
+            string s1 = doubledChromosomes[idx].getDNA().first;
+            string s2 = doubledChromosomes[idx].getDNA().second;
+            selectedChromosomes.push_back({s1, s2});
+        }
+        similarity = calculateGeneticSimilarity(Animal(selectedChromosomes));
+    }
+
+    Animal offspring = Animal(selectedChromosomes);
+
+    return offspring;
+}
+
+Animal Animal::operator+(Animal &other)
+{
+    Animal offspring1 = this->asexualReproduction();
+    Animal offspring2 = other.asexualReproduction();
+
+    vector<pair<string, string>> combinedChromosomes;
+    double similarity1 = 0.0;
+    double similarity2 = 0.0;
+
+    while (similarity1 <= 70.0 && similarity2 <= 70.0)
+    {
+        combinedChromosomes.clear();
+
+        for (size_t i = 0; i < offspring1.chromosomes.size() / 2; i++)
+        {
+            int idx = rand() % offspring1.chromosomes.size();
+            string s1 = offspring1.chromosomes[idx].getDNA().first;
+            string s2 = offspring1.chromosomes[idx].getDNA().second;
+            combinedChromosomes.push_back({s1, s2});
+        }
+
+        for (size_t i = 0; i < offspring2.chromosomes.size() / 2; i++)
+        {
+            int idx = rand() % offspring2.chromosomes.size();
+            string s1 = offspring2.chromosomes[idx].getDNA().first;
+            string s2 = offspring2.chromosomes[idx].getDNA().second;
+            combinedChromosomes.push_back({s1, s2});
+        }
+
+        Animal child = Animal(combinedChromosomes);
+        similarity1 = this->calculateGeneticSimilarity(child);
+        similarity2 = other.calculateGeneticSimilarity(child);
+    }
+
+    Animal child = Animal(combinedChromosomes);
+    return child;
+}
--- includes/Animal.h
+++ includes/Animal.h
@@ -10,17 +10,30 @@ class Animal : public Cell
 {
 public:
     Animal(const vector<pair<string, string>> &chromosomeDNA);
-    /* Initializes the Animal object with the provided chromosome DNA pairs 
+    /* Initializes the Animal object with the provided chromosome DNA pairs
     by calling the constructor of the Cell class,which handles the chromosome storage. */
 
     double calculateGeneticSimilarity(const Animal &other) const;
-    /* This method counts matching chromosomes across both strands 
+    /* This method counts matching chromosomes across both strands
     and divides that count by the total number of positions to return a similarity percentage. */
 
     bool operator==(const Animal &other) const;
     /* Overloads the equality operator to determine if two Animal objects are of the same species.
     It first checks if the number of chromosomes is identical,
     and then it uses the calculateGeneticSimilarity method to see if the genetic similarity exceeds 70%.*/
+
+    Animal asexualReproduction();
+    /* This method implements asexual reproduction by duplicating the chromosomes
+    and creating a new Animal from a random selection of these duplicated chromosomes.
+    It ensures that the resulting offspring has a genetic similarity of more than 70% with its parent.
+    This method uses a loop to randomly select chromosomes and calculates the genetic similarity
+    until the desired threshold is met, simulating a natural reproduction process. */
+
+    Animal operator+(Animal &other);
+    /* Overloads the + operator to facilitate sexual reproduction between two Animal objects.
+    It performs asexual reproduction on both parents to generate two offspring,
+    then randomly selects half of the chromosomes from each offspring to create a new child.
+    It checks the genetic similarity of the child against both parents to ensure it exceeds 70%. */
 };
 
 #endif
