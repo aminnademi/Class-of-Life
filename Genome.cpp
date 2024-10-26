b78a834f6e620a22ee9f4528526cc156373cb085
Amin Nademi <mgamer7557@gmail.com>
2024-10-26 21:06:02 +0330

    Implement mutation methods according to the specified rules.

--- Genome.cpp
+++ Genome.cpp
@@ -58,3 +58,95 @@ string Genome::getCompFromRNA()
     string comp = getCompStrand(RNA);
     return comp;
 }
+
+void Genome::smallMutation(char from, char to, int n)
+{
+    int count = 0;
+    for (char &base : RNA)
+    {
+        if (base == from && count < n)
+        {
+            base = to;
+            count++;
+        }
+    }
+
+    count = 0;
+
+    for (int i = 0; i < DNA.first.length(); i++)
+    {
+        if (DNA.first[i] == from && count < n)
+        {
+            DNA.first[i] = to;
+            DNA.second[i] = getCompBase(to);
+            count++;
+        }
+        else if (DNA.second[i] == from && count < n)
+        {
+            DNA.second[i] = to;
+            DNA.first[i] = getCompBase(to);
+            count++;
+        }
+    }
+}
+
+void Genome::largeMutation(const string &S1, const string &S2)
+{
+    size_t rnaPos = RNA.find(S1);
+
+    if (rnaPos != string::npos)
+        RNA.replace(rnaPos, S1.length(), S2);
+
+    size_t dna1Pos = DNA.first.find(S1);
+    size_t dna2Pos = DNA.second.find(S1);
+
+    if (dna1Pos != string::npos)
+    {
+        DNA.first.replace(dna1Pos, S1.length(), S2);
+        string comp = getCompStrand(S2);
+        DNA.second.replace(dna1Pos, S1.length(), comp);
+    }
+    else
+    {
+        DNA.second.replace(dna2Pos, S1.length(), S2);
+        string comp = getCompStrand(S2);
+        DNA.first.replace(dna2Pos, S1.length(), comp);
+    }
+}
+
+void Genome::reverseMutation(const string &S1)
+{
+
+    string newS1 = S1;
+    size_t rnaPos = RNA.find(S1);
+
+    if (rnaPos != string::npos)
+    {
+        reverse(newS1.begin(), newS1.end());
+        RNA.replace(rnaPos, S1.length(), newS1);
+    }
+
+    size_t dna1Pos = DNA.first.find(S1);
+    size_t dna2Pos = DNA.second.find(S1);
+
+    if (dna1Pos != string::npos)
+    {
+        string newS1 = S1;
+        reverse(newS1.begin(), newS1.end());
+        DNA.first.replace(dna1Pos, S1.length(), newS1);
+
+        string comp = getCompStrand(S1);
+        reverse(comp.begin(), comp.end());
+        DNA.second.replace(dna1Pos, S1.length(), comp);
+    }
+    else
+    {
+        string newS1 = S1;
+        reverse(newS1.begin(), newS1.end());
+        DNA.second.replace(dna2Pos, S1.length(), newS1);
+
+        string comp = getCompStrand(S1);
+        reverse(comp.begin(), comp.end());
+        DNA.first.replace(dna2Pos, S1.length(), comp);
+    }
+}
--- includes/Genome.h
+++ includes/Genome.h
@@ -18,7 +18,7 @@ private:
     // Helper function to get the complementary base by the biology rules.
 
     string getCompStrand(const string &strand);
-    // Helper function to create the complementary string for a given strand using 'getCompBase'. 
+    // Helper function to create the complementary string for a given strand using 'getCompBase'.
 
 public:
     Genome();
@@ -33,7 +33,7 @@ public:
     Genome(const string &rna);
     // Alternate constructor for RNA-only initialization (used in Virus)
 
-        void printGeneticContents() const;
+    void printGeneticContents() const;
     // A convenient method for displaying the full genetic information of an organism.
 
     string getRNA() const;
@@ -45,6 +45,23 @@ public:
 
     string getCompFromRNA();
     // Computes and returns the complementary strand of the RNA using 'getCompStrand'.
+
+    void smallMutation(char from, char to, int n);
+    /* This method iterates through each base in the RNA & DNA. For each base in strands,
+    when it encounters a base matching 'from', it replaces it with 'to' and increments the mutation count until it reaches 'n' mutations,
+    ensuring the complementary base in the other DNA strand is updated to reflect the mutation. */
+
+    void largeMutation(const string &S1, const string &S2);
+    /* This method first searches for S1 in the RNA strand & replaces the first occurrence of S1 with S2.
+    Next, it searches for S1 in the two DNA strands. If S1 is found in the first DNA strand,
+    it replaces it with S2 and updates the second strand by replacing the same section with the complementary string of S2.
+    If S1 is found in the second strand, the roles reverse. */
+
+    void reverseMutation(const string &S1);
+    /* This method locates S1 in the RNA strand. If found, it reverses the substring and replaces it.
+    Then It searches for S1 in the DNA strands similarly.
+    When S1 is found in the first strand, it reverses it and replaces the occurrence in the first strand
+    while also reversing its complementary sequence in the second strand. The process is mirrored if S1 is found in the second strand. */
 };
 
 #endif
